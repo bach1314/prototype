4864453f55d50844a637826ac3eeee6f4fe4d650
bach1314 <deny@chefti.com>
2014-02-27 05:24:23 -0800

    Add Main - SQL Login

--- protected/config/main.php
+++ protected/config/main.php
@@ -47,19 +47,21 @@ return array(
 			),
 		),
 		*/
+        /*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
+        */
 		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
+
+
+        'db'=>array(
+            'connectionString' => 'pgsql:host=localhost;dbname=postgres',
+            'username' => 'postgres',
+            'password' => '1q3e1q4r',
+            'charset' => 'utf8',
+        ),
+
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
